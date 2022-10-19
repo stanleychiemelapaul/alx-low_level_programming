db1b9ace7057fbbc0b7bcde161df930b78b3409c
Stanley Chiemela Paul <stanleypaul313@gmail.com>
2022-10-19 14:10:57 -0700

    edited 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,6 +9,7 @@
 void print_times_table(int n)
 {
 	int i, j, k;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -43,7 +44,7 @@ void print_times_table(int n)
 				}
 
 			}
-			putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
