db0e308e4864ed8516caf34a2cf4008feefdccf6
Stanley Chiemela Paul <stanleypaul313@gmail.com>
2022-10-20 16:41:40 -0700

    8-print_square.c file

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,14 +7,20 @@
 void print_square(int size)
 {
 	if (size <= 0)
+	{
 		_putchar('\n');
-	else
+	} else
+	{
 		int i, j;
 
 		for (i = 0; i < size; i++)
+		{
 			for (j = 0; j < size; j++)
+			{
 				_putchar('#');
+			}
 
 			_putchar('\n');
-
+		}
+	}
 }
