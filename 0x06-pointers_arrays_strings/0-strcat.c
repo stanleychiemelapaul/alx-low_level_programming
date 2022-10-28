2b2cb70bffa0a2822c5c8ce1a28ed2473fff8f86
Stanley Chiemela Paul <stanleypaul313@gmail.com>
2022-10-27 22:07:24 -0700

    0-strcat.c readded

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,16 +7,22 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, n;
+	int count = 0, count2 = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	while (*(dest + count) != '\0')
 	{
+		count++;
 	}
 
-	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	while (count2 >= 0)
 	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+
 	}
 
 	return (dest);
-
 }
