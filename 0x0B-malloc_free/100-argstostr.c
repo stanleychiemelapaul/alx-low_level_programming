51b293ddd23afca11bf535f9b8567a7b89713709
Stanley Chiemela Paul <stanleypaul313@gmail.com>
2022-11-08 15:48:00 -0800

    adding 101-strtow.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+#include <stdlib.h>
+
+/**
+* argstostr - concatenates all the arguments of a program.
+* @ac: argument count.
+* @av: argument vector.
+*
+* Return: pointer of an array of char
+*/
+char *argstostr(int ac, char **av)
+{
+	char *aout;
+	int c, i, j, ia;
+
+	if (ac == 0)
+		return (NULL);
+
+	for (c = i = 0; i < ac; i++)
+	{
+		if (av[i] == NULL)
+			return (NULL);
+
+		for (j = 0; av[i][j] != '\0'; j++)
+			c++;
+		c++;
+	}
+	aout = malloc((c + 1) * sizeof(char));
+	
+	if (aout == NULL)
+	
