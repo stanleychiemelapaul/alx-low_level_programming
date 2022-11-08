97dbb31f9abbc48a1a85bf905ab319b03ac93624
Stanley Chiemela Paul <stanleypaul313@gmail.com>
2022-11-08 15:53:16 -0800

    adding 100... file

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -27,6 +27,26 @@ char *argstostr(int ac, char **av)
 		c++;
 	}
 	aout = malloc((c + 1) * sizeof(char));
-	
+
 	if (aout == NULL)
-	
+	{
+		free(aout);
+		return (NULL);
+	}
+
+	for (i = j = ia = 0; ia < c; j++, ia++)
+	{
+		if (av[i][j] == '\0')
+		{
+			aout[ia] = '\n';
+			i++;
+			ia++;
+			j = 0;
+		}
+		if (ia < c - 1)
+			aout[ia] = av[i][j];
+	}
+
+	aout[ia] = '\0';
+	return (aout);
+}	
