a78c6d1ba2dd24b848c5e9227e1187a4143405d3
Stanley Chiemela Paul <stanleypaul313@gmail.com>
2022-11-10 19:58:39 -0800

    readd 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -79,11 +79,9 @@ int main(int argc, char *argv[])
 	char *nout;
 
 	if (argc != 3)
-		printf("Error\n");
-		exit(98);
+		printf("Error\n"), exit(98);
 
-	ln1 = _checknum(argv, 1);
-       	ln2 = _checknum(argv, 2);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
 
 	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 	if (nout == NULL)
@@ -112,9 +110,7 @@ int main(int argc, char *argv[])
 				break;
 			lnout--;
 
-			free(nout);
-			nout = malloc(lnout + 1);
-			nout = _initialize_array(nout, lnout);
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
 			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 		}
 
