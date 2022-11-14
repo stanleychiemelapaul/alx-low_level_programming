88bad9414506b549f13da180766429a4a3a52e1f
Stanley Chiemela Paul <stanleypaul313@gmail.com>
2022-11-14 13:18:12 -0800

    adding files

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+* init_dog - initializes a variable of type struct dog.
+*
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
