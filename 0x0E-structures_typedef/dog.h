782c22e91991a01647f37601eec1fc3dfb1a927a
Stanley Chiemela Paul <stanleypaul313@gmail.com>
2022-11-14 13:21:28 -0800

    readding dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
 * dog_t - Typedef for struct dog
@@ -23,4 +25,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
+#endif
