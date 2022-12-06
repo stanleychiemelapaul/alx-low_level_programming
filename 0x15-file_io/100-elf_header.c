e0fd4a80a2bf53b77d2dab4e871be9f5f249fade
Stanley Chiemela Paul <stanleypaul313@gmail.com>
2022-12-05 21:09:15 -0800

    Edited 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -183,3 +183,69 @@ void check_sys(char *ptr)
 	print_type(ptr);
 	print_addr(ptr);
 }
+
+/**
+* check_elf - check if it is an elf file.
+* @ptr: magic.
+* Return: 1 if it is an elf file. 0 if not.
+*/
+
+int check_elf(char *ptr)
+{
+	int addr = (int)ptr[0];
+	char E = ptr[1];
+	char L = ptr[2];
+	char F = ptr[3];
+
+	if (addr == 127 && E == 'E' && L == 'L' && F == 'F')
+		return (1);
+	return (0);
+}
+
+/**
+* main - program that displays the information 
+* contained in the ELF header
+* at the start of an ELF file.
+*
+* Return: Always 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	int fd, ret_read;
+	char ptr[27];
+
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
+		exit(98);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+
+	if (fd < 0)
+	{
+		dprintf(STDERR_FILENO, "Err: file can not be open\n");
+		exit(98);
+	}
+
+	lseek(fd, 0, SEEK_SET);
+	ret_read = read(fd, ptr, 27);
+
+	if (ret_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Err: The file can not be read\n");
+		exit(98);
+	}
+
+	if (!check_elf(ptr))
+	{
+		dprintf(STDERR_FILENO, "Err: It is not an ELF\n");
+		exit(98);
+	}
+
+	check_sys(ptr);
+	close(fd);
+
+	return (0);
+}
