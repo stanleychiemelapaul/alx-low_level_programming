5516f6e573acc7ab16bfcc6ae85ea5743bd91cad
Stanley Chiemela Paul <stanleypaul313@gmail.com>
2023-01-17 13:49:43 -0800

    edited for betty style compliance

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -4,83 +4,103 @@
 #include <stdlib.h>
 #include <string.h>
 
-int _putchar(char c) {
+int _putchar(char c)
+{
     putchar(c);
     return 0;
 }
 
-int _islower(int c) {
+int _islower(int c)
+{
     return islower(c);
 }
 
-int _isalpha(int c) {
+int _isalpha(int c)
+{
     return isalpha(c);
 }
 
-int _abs(int n) {
+int _abs(int n)
+{
     return abs(n);
 }
 
-int _isupper(int c) {
+int _isupper(int c)
+{
     return isupper(c);
 }
 
-int _isdigit(int c) {
+int _isdigit(int c)
+{
     return isdigit(c);
 }
 
-int _strlen(char *s) {
+int _strlen(char *s)
+{
     return strlen(s);
 }
 
-void _puts(char *s) {
+void _puts(char *s)
+{
     puts(s);
 }
 
-char *_strcpy(char *dest, char *src) {
+char *_strcpy(char *dest, char *src)
+{
     return strcpy(dest, src);
 }
 
-int _atoi(char *s) {
+int _atoi(char *s)
+{
     return atoi(s);
 }
 
-char *_strcat(char *dest, char *src) {
+char *_strcat(char *dest, char *src)
+{
     return strcat(dest, src);
 }
 
-char *_strncat(char *dest, char *src, int n) {
+char *_strncat(char *dest, char *src, int n)
+{
     return strncat(dest, src, n);
 }
 
-char *_strncpy(char *dest, char *src, int n) {
+char *_strncpy(char *dest, char *src, int n)
+{
     return strncpy(dest, src, n);
 }
 
-int _strcmp(char *s1, char *s2) {
+int _strcmp(char *s1, char *s2)
+{
     return strcmp(s1, s2);
 }
 
-char *_memset(char *s, char b, unsigned int n) {
+char *_memset(char *s, char b, unsigned int n)
+{
     return memset(s, b, n);
 }
 
-char *_memcpy(char *dest, char *src, unsigned int n) {
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
     return memcpy(dest, src, n);
 }
 
-char *_strchr(char *s, char c) {
+char *_strchr(char *s, char c)
+{
     return strchr(s, c);
 }
 
-unsigned int _strspn(char *s, char *accept) {
+unsigned int _strspn(char *s, char *accept)
+{
     return strspn(s, accept);
 }
 
-char *_strpbrk(char *s, char *accept) {
+char *_strpbrk(char *s, char *accept)
+{
     return strpbrk(s, accept);
 }
 
-char *_strstr(char *haystack, char *needle) {
+char *_strstr(char *haystack, char *needle)
+{
     return strstr(haystack, needle);
 }
